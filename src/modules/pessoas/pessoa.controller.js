f2a21415a4f1638d4e60653cb4f0ce7e6ac06fd7
Kaline Cerqueira <kalicerq@gmail.com>
2026-02-22 14:56:01 -0300

    feat: rotas de pessoas e regra de exclusão finalizadas

--- src/modules/pessoas/pessoa.controller.js
+++ src/modules/pessoas/pessoa.controller.js
@@ -1,1 +1,30 @@
 // coordena http
+
+import { PessoaService } from './pessoa.service.js';
+
+const service = new PessoaService();
+
+export class PessoaController {
+  // rota: PUT /pessoas/:id [cite: 40]
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      const data = req.body;
+      const result = await service.updatePessoa(id, data);
+      return res.status(200).json(result);
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+  }
+
+  // rota: DELETE /pessoas/:id [cite: 40]
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      await service.deletePessoa(id);
+      return res.status(204).send(); // sucesso sem conteúdo
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+  }
+}
---
+++ src/modules/pessoas/pessoa.repository.js
@@ -0,0 +1,23 @@
+import prisma from '../../config/PrismaClient.js';
+
+export class PessoaRepository {
+    async findById(id) {
+        return await prisma.pessoa.findUnique({
+            where: { id: parseInt(id) },
+            include: { conhecimentos: true }
+        });
+    }
+
+    async update(id, data) {
+        return await prisma.pessoa.update({
+            where: { id: parseInt(id) },
+            data
+        });
+    }
+
+    async delete(id) {
+        return await prisma.pessoa.delete({
+            where: { id: parseInt(id) }
+        });
+    }
+}
