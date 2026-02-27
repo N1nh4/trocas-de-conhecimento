6d92eac8d4901a351e03850494afbbe9461dd247
Rhobertta Cardoso <rgrasielle1@gmail.com>
2026-02-27 10:35:38 -0300

    Adiciona configuração de CORS para integração com frontend

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 import pessoaRoutes from './modules/pessoas/pessoa.route.js';
 import conhecimentoRoutes from './modules/conhecimentos/conhecimento.route.js';
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 
 app.use(express.json());
 
